6fe2e2fc0f214d2dc4cc4f0c89c24bac1b414fdf
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-10-14 23:30:23 +0200

    fix bug

--- script/transformPath.ts
+++ script/transformPath.ts
@@ -2,13 +2,8 @@ export const transformPath =
   ([origin, target]: [string, string]) =>
   (path: string) => {
     if (path.startsWith(origin) || path.startsWith(`/${origin}`)) {
-      if (path.startsWith(`/${origin}`)) {
-        path = path.replace("/", "");
-        origin = origin.slice(0, -1);
-        console.log(origin);
-
-        return path.replace(origin, target);
-      } else return path.replace(origin, target);
+      target = `${target}/`;
+      return path.replace(origin, target);
     }
     return path;
   };
